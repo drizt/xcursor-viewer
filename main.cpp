92e53bfea5dd8fa5e45b7c6d8f329a1722064ba1
Ivan Romanov <drizt72@zoho.eu>
2026-01-23 01:19:32 +0500

    Add SPDX licenses

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
-/*
- * Copyright (C) 2019  Ivan Romanov <drizt72@zoho.eu>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+// SPDX-FileCopyrightText: 2019, 2026 Ivan Romanov <drizt72@zoho.eu>
+// SPDX-FileContributor: 2020 Ruslan Kabatsayev <b7.10110111@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "dialog.h"
 
